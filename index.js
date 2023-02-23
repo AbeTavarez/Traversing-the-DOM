9e36a3b0e922bff99ea2a0009ba3c633ef315262
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-02-22 23:49:58 -0500

    ding

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//* 1. create a variable named (root) and select the html tag from the html file using querySelector
+//TODO: 1. Create a variable named (root) and select the html tag from the html file using querySelector
 
-// add the code here
+//* add the code here
 
 // console log the root variable
 // check the difference between:
@@ -9,51 +9,51 @@
 // console.log(root.children);
 // console.log(root.childNodes);
 
-//* 2. Create a variable named (body), using the root array from above assign the body element to the body variable.
+//TODO: 2. Create a variable named (body), using the root array from above assign the body element to the body variable.
 
-// add the code here
+//* add the code here
 
 // log the variable (body)
 // console.log(body);
 
-//* 3. Create a variable named (h1), using the (body) variable fom above use the property (firstElementChild) to assign the h1 element to the (h1) variable.
+//TODO: 3. Create a variable named (h1), using the (body) variable fom above use the property (firstElementChild) to assign the h1 element to the (h1) variable.
 
-// add the code here
+//* add the code here
 
 // log the h1 variable to se the result
 // console.log(h1);
 
-//* 4. Create a variable named (ul), using the (h1) variable from above use the (nextElementSibling) property to assign the value to the (ul) variable.
+//TODO: 4. Create a variable named (ul), using the (h1) variable from above use the (nextElementSibling) property to assign the value to the (ul) variable.
 
-// add the code here
+//* add the code here
 
 // Log the ul variable to se the result
 // console.log(ul);
 
-//* 5. Create a variable named (thirdLi), using the (ul) variable from above use (lastElementChild) property to assign the value to the (thirdLi) variable.
+//TODO: 5. Create a variable named (thirdLi), using the (ul) variable from above use (lastElementChild) property to assign the value to the (thirdLi) variable.
 
-// add the code here
+//* add the code here
 
 // Log the text content of the li -> (Third li 🎃)
 // console.log(thirdLi.textContent);
 
-//* 6. Create a variable named (secondLi), using the (thirdLi) variable from above use (previousElementSibling) property to assign the value to the (secondLi) variable.
+//TODO: 6. Create a variable named (secondLi), using the (thirdLi) variable from above use (previousElementSibling) property to assign the value to the (secondLi) variable.
 
-// add the code here
+//* add the code here
 
 // Log the text content of the li -> (Second li ⛄)
 // console.log(secondLi.textContent);
 
-//* 7. Create a variable named (firstLi), using the (thirdLi) variable from above use (previousElementSibling.previousElementSibling) properties to assign the value to the (firstLi) variable.
+//TODO: 7. Create a variable named (firstLi), using the (thirdLi) variable from above use (previousElementSibling.previousElementSibling) properties to assign the value to the (firstLi) variable.
 
-// add the code here
+//* add the code here
 
 // Log the text content of the li -> (First li 🦁)
 // console.log(firstLi.textContent);
 
-//* 8. Finally create a variable named (backToTheUl) using the thirdLi variable from above, use the (parentElement) property to assign th value to the (backToTheUl) variable.
+//TODO: 8. Finally create a variable named (backToTheUl) using the thirdLi variable from above, use the (parentElement) property to assign th value to the (backToTheUl) variable.
 
-// add the code here
+//* add the code here
 
 // Log the backToTheUl -> (ul)
 // console.log(backToTheUl);
